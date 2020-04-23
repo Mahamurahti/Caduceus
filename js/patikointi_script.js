d1e9a78667513fd1325c94fc45f7e2c436afc320
Mahamurahti <dagility1@gmail.com>
2020-04-23 13:11:50 +0300

    Added Leaflet map and got fetch to print data from lipas

---
+++ js/patikointi_script.js
@@ -0,0 +1,41 @@
+'use strict';
+
+let currentPos = null;
+
+const map = L.map('mapid');
+
+function showMap(crd) {
+  map.setView([crd.latitude, crd.longitude], 14);
+}
+
+function getPos(pos){
+  currentPos = pos.coords;
+  showMap(currentPos);
+}
+
+function error(err){
+  console.warn(`ERROR(${err.code}): ${err.message}`);
+}
+
+navigator.geolocation.getCurrentPosition(getPos, error);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+}).addTo(map);
+
+//------------------------------------------------------------------------------//
+let proxyUrl = 'https://cors-anywhere.herokuapp.com/',
+    targetUrl = 'http://lipas.cc.jyu.fi/api/sports-place-types?lang=fi';
+fetch(proxyUrl + targetUrl)
+.then(function(response) {
+  return response.json();
+})
+.then(function(data) {
+  console.table(data);
+  document.querySelector("pre").innerHTML = JSON.stringify(data, null, 2);
+  return data;
+})
+.catch(function(error) {
+  console.log('Error: ' + error);
+});
+//------------------------------------------------------------------------------//
